66bf916d6bef27c3b627bc2df4b9546810cf17ea
flygoast <flygoast@126.com>
2013-10-22 00:03:52 +0800

    optimise loops in ngx_http_upstream_init_ketama_chash()

--- ngx_http_upstream_ketama_chash_module.c
+++ ngx_http_upstream_ketama_chash_module.c
@@ -205,11 +205,11 @@ ngx_http_upstream_init_ketama_chash(ngx_conf_t *cf,
     n = 0;
 
     for (i = 0; i < us->servers->nelts; i++) {
-        for (j = 0; j < server[i].naddrs; j++) {
-            if (server[i].backup) {
-                continue;
-            }
+        if (server[i].backup) {
+            continue;
+        }
 
+        for (j = 0; j < server[i].naddrs; j++) {
             peers->peer[n].sockaddr = server[i].addrs[j].sockaddr;
             peers->peer[n].socklen = server[i].addrs[j].socklen;
             peers->peer[n].name = server[i].addrs[j].name;
@@ -265,11 +265,11 @@ ngx_http_upstream_init_ketama_chash(ngx_conf_t *cf,
     n = 0;
 
     for (i = 0; i < us->servers->nelts; i++) {
-        for (j = 0; j < server[i].naddrs; j++) {
-            if (!server[i].backup) {
-                continue;
-            }
+        if (!server[i].backup) {
+            continue;
+        }
 
+        for (j = 0; j < server[i].naddrs; j++) {
             backup->peer[n].sockaddr = server[i].addrs[j].sockaddr;
             backup->peer[n].socklen = server[i].addrs[j].socklen;
             backup->peer[n].name = server[i].addrs[j].name;
